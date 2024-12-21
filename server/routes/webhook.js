cda8d0591c142171489a59ae3d95a1ee0b9a12b3
Peter Bishop <pjb.den@gmail.com>
2024-12-20 22:09:50 -0700

    typo

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -7,7 +7,7 @@ const { error } = require("console");
 router.post('/webhook', async (req, res) => {
     const key = process.env.WEBHOOK_SECRET;
     const body = JSON.stringify(req.body);
-    const hash = crypto.createHmac('sha2565', key).update(body);
+    const hash = crypto.createHmac('sha256', key).update(body);
     const signature = hash.digest('hex');
 
     try {
@@ -19,7 +19,7 @@ router.post('/webhook', async (req, res) => {
         }
 
         console.log('received x-secret', secretValue);
-        console.log(`Compare: [${key}] vs [${secretValue}]`);
+        console.log(`Compare: [${signature}] vs [${secretValue}]`);
 
         res.status(200).json({
             message: 'Payload Received and validated',
